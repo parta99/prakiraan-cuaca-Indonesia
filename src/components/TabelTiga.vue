4ec83445ee9d70796b3d12ed74cc50afcf394a28
DE Juniada <dejon933@gmail.com>
2023-04-29 16:20:16 +0800

    Create TabelTiga.vue

---
+++ src/components/TabelTiga.vue
@@ -0,0 +1,13 @@
+<script setup>
+import JudulItem from './JudulItem.vue'
+import TabelItem from './TabelItemTiga.vue'
+</script>
+
+<template>
+    <JudulItem>
+        <template #heading>Prakiraan Cuaca Indonesia</template>
+        <a href="https://data.bmkg.go.id/prakiraan-cuaca/" target="_blank" rel="noopener">BMKG</a> 
+        <template #paragraf> (Badan Meteorologi, Klimatologi, dan Geofisika) sebagai sumber data</template>
+    </JudulItem>
+    <TabelItem/>
+</template>
