6110227650b2b0ef2a07d19bc2c17c3c8340fed7
DE Juniada <dejon933@gmail.com>
2023-04-30 19:10:04 +0800

    Update TabelSatu.vue

--- src/components/TabelSatu.vue
+++ src/components/TabelSatu.vue
@@ -7,7 +7,8 @@ import TabelItem from './TabelItemSatu.vue'
     <JudulItem>
         <template #heading>Prakiraan Cuaca Indonesia</template>
         <a href="https://data.bmkg.go.id/prakiraan-cuaca/" target="_blank" rel="noopener">BMKG</a> 
-        <template #paragraf> (Badan Meteorologi, Klimatologi, dan Geofisika) sebagai sumber data</template>
+        <template #paragraf> (Badan Meteorologi, Klimatologi, dan Geofisika) sebagai sumber data</template>    
+        <template #h3> data di update setiap 3 hari</template>    
     </JudulItem>
     <TabelItem/>
 </template>
