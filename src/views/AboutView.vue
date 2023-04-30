8515fa6a6a019357ac6afeba7770edab19140635
DE Juniada <dejon933@gmail.com>
2023-04-30 22:21:07 +0800

    update
    update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,18 +1,15 @@
-<template>
-  <div class="about px-8 py-8">
-    <h1 class=" font-bold py-2">Aplikasi Cuaca Indonesia</h1>
-    <p>Aplikasi Cuaca Indonesia adalah sebuah aplikasi yang menampilkan informasi tentang perkiraan cuaca di berbagai kota dan provinsi di Indonesia berdasarkan data dari BMKG (Badan Meteorologi, Klimatologi, dan Geofisika).</p>
-    <p>Aplikasi ini menyajikan informasi yang akurat dan terpercaya tentang kondisi cuaca saat ini dan perkiraan cuaca beberapa hari ke depan. Informasi yang ditampilkan meliputi suhu udara, dan juga kondisi cuaca seperti cerah, hujan, awan, dan lain sebagainya.</p>
-  </div>
-</template>
+<script setup>
+import JudulItem from '../components/JudulItem.vue'
+</script>
 
-<style scoped>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-    padding-left: 4rem;
-  }
-}
-</style>
+<template>
+  <main>
+    <JudulItem>
+          <template #heading>Prakiraan Cuaca Indonesia</template> 
+          <template #paragraf>Aplikasi Cuaca Indonesia adalah sebuah aplikasi yang menampilkan informasi tentang perkiraan cuaca di berbagai kota dan provinsi di Indonesia berdasarkan data dari <a href="https://data.bmkg.go.id/prakiraan-cuaca/" target="_blank" rel="noopener">BMKG </a>  (Badan Meteorologi, Klimatologi, dan Geofisika).</template>
+      </JudulItem>
+      <JudulItem>
+          <template #paragraf>Aplikasi ini menyajikan informasi yang akurat dan terpercaya tentang kondisi cuaca saat ini dan perkiraan cuaca beberapa hari ke depan. Informasi yang ditampilkan meliputi suhu udara, kelembaban, kecepatan angin, dan juga kondisi cuaca seperti cerah, hujan, awan, dan lain sebagainya. Dengan adanya aplikasi Cuaca Indonesia ini, masyarakat dapat dengan mudah mengakses informasi terkait kondisi cuaca dan kualitas udara di wilayah tempat tinggal mereka atau di tempat-tempat yang ingin mereka kunjungi. Aplikasi ini membantu masyarakat untuk lebih waspada dan siap menghadapi kondisi cuaca yang mungkin tidak diinginkan dan memperhatikan kesehatan dan lingkungan sekitar.</template>
+      </JudulItem>
+  </main>
+</template>
