6cf4bcaf0cd96da4b4cd4801cb1a2cb0535f1041
DE Juniada <dejon933@gmail.com>
2023-05-04 11:53:45 +0800

    up
    less

--- src/components/TabelSatu.vue
+++ src/components/TabelSatu.vue
@@ -8,7 +8,7 @@ import WaktuUtc from './WaktuUtc.vue';
     <JudulItem>
         <template #heading>Prakiraan Cuaca Indonesia</template>
         <a href="https://data.bmkg.go.id/prakiraan-cuaca/" target="_blank" rel="noopener">BMKG</a> 
-        <template #paragraf> (Badan Meteorologi, Klimatologi, dan Geofisika) sebagai sumber data</template>     
+        <template #paragraf> (Badan Meteorologi, Klimatologi, dan Geofisika) sebagai sumber data.</template>     
     </JudulItem>
     <WaktuUtc />
     <TabelItem/>
